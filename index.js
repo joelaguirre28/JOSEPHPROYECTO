2ac30aba5ad8388f9dff3c883999a1b287682ad9
Your Name <you@example.com>
2024-03-12 22:06:34 -0500

    Casi terminado

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ function getData(){
     fetch(URL)
     .then( response => response.json() )
     .then( data => {
+        let productosCardSet = JSON.stringify(data)
+        localStorage.setItem('productos', productosCardSet)
+        console.log("productosCardSet: ",productosCardSet)
+
+        let productosCardGet = localStorage.getItem('productos')
+        let listaProducto = JSON.parse(productosCardGet)
+        console.log("listaProducto: ",listaProducto)
+
 
         for(let i = 0; i < data.length; i++ ){
             //rgb(r g b)
             let r =Math.floor(Math.random()*256)
             let g =Math.floor(Math.random()*256)
             let b =Math.floor(Math.random()*256)
+            let precio = data[i].price
             console.log("colo r:", r , g , b)
 
              let bloqueHtml = 
@@ -22,7 +31,7 @@ function getData(){
                 </div>
                 
                 <p class='titulo'>${data[i].title}</p>
-                <p>$${data[i].price}</p>
+                <p>$${precio} <span class='precio-sd'>$${((precio * 0.1) + precio).toFixed(2)}</span></p>
                 <label class='categoria'>${data[i].category}</label>
              </div>
              `;
