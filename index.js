bf28cd0fd0e6348b0d679ef598189c1366c9c3fc
Your Name <you@example.com>
2024-03-20 20:53:12 -0500

    avanceproyecto

--- index.js
+++ index.js
@@ -1,56 +1,55 @@
-// URL de la API que proporciona los productos
 const URL = "https://fakestoreapi.com/products";
 
-// Elemento HTML donde se mostrarán los productos
+
 let productos = document.getElementById("productos");
 
-// Elemento HTML del overlay que contiene el formulario
+
 let overlay = document.getElementById("overlay");
 
-// Elemento HTML del botón para mostrar el formulario
+
 let mostrarFormularioBtn = document.getElementById("mostrar-formulario");
 
-// Elemento HTML del botón para cerrar el formulario
+
 let cerrarFormularioBtn = document.getElementById("cerrar-formulario");
 
-// Función para obtener los datos de la API y mostrar los productos
+
 function getData() {
-    // Obtener los productos almacenados localmente
+    
     let productosGuardados = JSON.parse(localStorage.getItem('productos'));
 
-    // Si hay productos almacenados localmente, mostrarlos
+    
     if (productosGuardados && productosGuardados.length > 0) {
         mostrarProductos(productosGuardados);
     }
 
-    // Obtener los datos de la API
+    
     fetch(URL)
         .then(response => response.json())
         .then(data => {
-            // Mostrar los productos de la API
+            
             mostrarProductos(data);
 
-            // Guardar los productos obtenidos de la API en el almacenamiento local
+            
             localStorage.setItem('productos', JSON.stringify(data));
         });
 }
 
-// Función para mostrar productos en la página
+
 function mostrarProductos(productosArray) {
-    // Inicializa una cadena que contendrá el HTML de la tabla de productos
+    
     let tabla = "<div class='card'>";
     
-    // Itera sobre el array de productos recibido como parámetro
+    
     for (let i = 0; i < productosArray.length; i++) {
-        // Genera valores aleatorios para los componentes de color RGB
+        
         let r = Math.floor(Math.random() * 256);
         let g = Math.floor(Math.random() * 256);
         let b = Math.floor(Math.random() * 256);
         
-        // Obtiene el precio del producto actual
+        
         let precio = productosArray[i].price;
         
-        // Construye el bloque HTML para cada producto
+        
         let bloqueHtml =
             `
             <div class='card-item'>
@@ -133,13 +132,13 @@ mostrarFormularioBtn.addEventListener("click", function() {
     overlay.style.display = "block";
 });
 
-// Ocultar formulario al hacer clic en el botón "Cerrar"
+
 cerrarFormularioBtn.addEventListener("click", function() {
     overlay.style.display = "none";
 });
 
-// Agregar evento de escucha para el envío del formulario
+
 document.getElementById("formulario-producto").addEventListener("submit", agregarProducto);
 
-// Llamar a la función getData para mostrar los productos iniciales
+
 getData();
